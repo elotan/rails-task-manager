55a7ecfaccb0ef789072cc88e28a8835b025956e
Anatole Gonon <anatole.gonon@gmail.com>
2018-03-27 16:02:34 +0200

    add tasks index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+#TODO : add a new route to list the tasks
+
+  root to: 'tasks#index'
+
+  get 'tasks', to: 'tasks#index'
+
+
+
 end
